9e9813bd964bc92366cde746ff7104c8148ffd3f
Daniel Lirio Reis <daniellirioreis@gmail.com>
2014-04-23 17:21:31 -0300

    Novos tipos de cursos(conversação)

--- app/enumerations/type_course.rb
+++ app/enumerations/type_course.rb
@@ -1,4 +1,6 @@
 class TypeCourse< EnumerateIt::Base
   associate_values :english            => 0,
-                   :Spanish            => 1
+                   :Spanish            => 1,
+                   :conversation_english => 2,
+                   :conversation_spanish => 3
 end
