f252b7e2c8104e43a5fe5d6456ddefb479e097d9
Daniel Lirio <daniellirioreis@gmail.com>
2014-07-15 14:22:03 -0300

    conferindo error

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -21,60 +21,56 @@ class Plan < ActiveRecord::Base
   private
 
   def create_schedules
-    if  new_record? 
-    
-      
-      if calendar.present?
-        days.each do |day|
-          case day_week
-            when Day::MONDAY_AND_WEDNESDAY
-              if day.day.wday == 1
-                schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
-                if schedule.save
-                end
+    if calendar.present?
+      days.each do |day|
+        case day_week
+          when Day::MONDAY_AND_WEDNESDAY
+            if day.day.wday == 1
+              schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
+              if schedule.save
               end
+            end
 
-              if day.day.wday == 3
-                schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
-                if schedule.save
-                end
+            if day.day.wday == 3
+              schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
+              if schedule.save
               end
-            when Day::TUESDAY_AND_THURSDAY
-              if day.day.wday == 2
-                schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
-                if schedule.save
-                end
+            end
+          when Day::TUESDAY_AND_THURSDAY
+            if day.day.wday == 2
+              schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
+              if schedule.save
               end
-              if day.day.wday == 4
-                schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
-                if schedule.save
-                end
+            end
+            if day.day.wday == 4
+              schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
+              if schedule.save
               end
-            when Day::SATURDAY
-              if day.day.wday == 6
-                schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
-                if schedule.save
-                end
+            end
+          when Day::SATURDAY
+            if day.day.wday == 6
+              schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
+              if schedule.save
               end
-            when Day::WEDNESDAY
-              if day.day.wday == 3
-                schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
-                if schedule.save
-                end
+            end
+          when Day::WEDNESDAY
+            if day.day.wday == 3
+              schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
+              if schedule.save
               end
-            when Day::MONDAY
-              if day.day.wday == 1
-                schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
-                if schedule.save
-                end
+            end
+          when Day::MONDAY
+            if day.day.wday == 1
+              schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
+              if schedule.save
               end
-            when Day::THURSDAY
-              if day.day.wday == 4
-                schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
-                if schedule.save
-                end
-              end            
-          end
+            end
+          when Day::THURSDAY
+            if day.day.wday == 4
+              schedule = Schedule.new(:calendar_day_id => day.id, :plan_id => id)
+              if schedule.save
+              end
+            end            
         end
       end
     end
