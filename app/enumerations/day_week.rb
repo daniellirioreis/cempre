0313524ad9e680881989046121e76552f04cae72
Daniel Lirio <daniellirioreis@gmail.com>
2014-08-04 13:38:56 -0300

    Melhora no calendario

---
+++ app/enumerations/day_week.rb
@@ -0,0 +1,12 @@
+class DayWeek < EnumerateIt::Base
+  associate_values :sunday                              => 0,
+                   :monday                              => 1,
+                   :tuesday                             => 2,
+                   :wednesday                           => 3,
+                   :thursday                            => 4,                   
+                   :friday                              => 5,
+                   :saturday                            => 6
+                   
+
+
+end
