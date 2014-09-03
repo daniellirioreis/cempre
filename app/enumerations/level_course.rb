3a2f7705c7f52d247edb3bb8388c7a25480a105f
Daniel Lirio <daniellirioreis@gmail.com>
2014-09-03 09:41:41 -0300

    add nivel pré-intermediario'

--- app/enumerations/level_course.rb
+++ app/enumerations/level_course.rb
@@ -1,5 +1,6 @@
 class LevelCourse< EnumerateIt::Base
   associate_values :elementary       => 0,
                    :intermediate     => 1,
-                   :advanced         => 2
+                   :advanced         => 2,
+                   :pre_intermediate => 3
 end
